7db49ea1a36a3b83c5880dfa9096f536c279c9dd
Zhang Yuan <zhang_yuan@franknetwork.co>
2025-03-14 11:16:56 +0800

    Update API endpoint, set up API functions in frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,7 +10,12 @@ model = mistral.mistral()
 def health_check():
     return {"message": "LLM API is up and running!"}
 
-@app.post("/query", summary="Query LLM")
+@app.post("/processText", summary="Query LLM - text input")
 def query_llm(input: str):
     diseases = process_diseases(model, input)
+    return {"response": diseases}
+
+@app.post("/processFile", summary="Query LLM - file input")
+def query_llm_file(file: bytes):
+    diseases = process_diseases(model, file.decode("utf-8"))
     return {"response": diseases}
